bf872afcf1ae6b1d11284262b9af5d6577d26f1f
geethasamynathan <geethasamynathan2011@gmail.com>
2026-06-05 12:57:39 +0530

    Nunit Demo Added

---
+++ CSharp Demos/UnitTEstingSolution/BankingConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using BankingConsoleApp.Models;
+using BankingConsoleApp.Services;
+namespace BankingConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccountService bankAccountService = new BankAccountService();
+            BankAccount bankAccount = bankAccountService.CreateAccount(
+                1001, "Geetha", 5000);
+
+            Console.WriteLine("Account Created successfully");
+            Console.WriteLine("Account Number : "+bankAccount.AccountNumber);
+            Console.WriteLine("Account Holder Name : " + bankAccount.AccountHolderName);
+            Console.WriteLine("Opening Balance : " + bankAccount.Balance);
+
+            bankAccountService .Deposit(bankAccount,4500);
+            Console.WriteLine($"Balance after Deposit : {bankAccount.Balance}");
+
+            bankAccountService.Withdraw(bankAccount,1000);
+            Console.WriteLine($"Balance after Withdarw : {bankAccount.Balance}");
+
+            decimal currentBalance = bankAccountService.GetBalance(bankAccount);
+            Console.WriteLine("Current Balance : " + currentBalance);
+
+            Console.ReadLine();
+        }
+    }
+
+   
+
+}
---
+++ CSharp Demos/UnitTEstingSolution/BankingConsoleApp/Services/BankAccountService.cs
@@ -0,0 +1,68 @@
+﻿using BankingConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingConsoleApp.Services
+{
+    public class BankAccountService
+    {
+        public BankAccount CreateAccount(int accountNumber, string accountHolderName, decimal openingBalance)
+        {
+            if (accountNumber <= 0)
+            {
+                throw new ArgumentException("Account Number must be greater tahn zero.");
+            }
+            if (string.IsNullOrWhiteSpace(accountHolderName))
+            {
+                throw new ArgumentException("Account holder name is required");
+            }
+            if (openingBalance <= 0)
+            {
+                throw new ArgumentException("Opening Balance caanot be negative.");
+            }
+            return new BankAccount(accountNumber, accountHolderName, openingBalance);
+        }
+
+        public void Deposit(BankAccount account, decimal amount)
+        {
+            if (account == null)
+            {
+                throw new ArgumentNullException(nameof(account), "Account cannot be null");
+            }
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount must be greater than zero.");
+            }
+            account.Balance += amount;
+        }
+
+
+        public void Withdraw(BankAccount account, decimal amount)
+        {
+            if (account == null)
+            {
+                throw new ArgumentNullException(nameof(account), "Account cannot be null");
+            }
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Withdraw amount must be greater than zero.");
+            }
+            if (amount > account.Balance)
+            {
+                throw new InvalidOperationException("Insufficient Balance.");
+            }
+            account.Balance -= amount;
+        }
+        public decimal GetBalance(BankAccount account)
+        {
+            if (account == null)
+            {
+                throw new ArgumentNullException(nameof(account), "Account cannot be null");
+            }
+            return account.Balance;
+        }
+    }
+}
