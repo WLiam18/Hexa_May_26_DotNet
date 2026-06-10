022a79d391780bc299b994000b0abbca488dce2d
geethasamynathan <geethasamynathan2011@gmail.com>
2026-06-10 12:25:06 +0530

    Fluent validation demo added

--- Asp Core Web API demos/ModelValidation/CourseRegistrationAPIDemo/Program.cs
+++ Asp Core Web API demos/ModelValidation/CourseRegistrationAPIDemo/Program.cs
@@ -1,20 +1,32 @@
+using CourseRegistrationAPIDemo.Dtos;
 using CourseRegistrationAPIDemo.Services;
+using FluentValidation;
+using CourseRegistrationAPIDemo.Validations;
+using FluentValidation.AspNetCore;
+    
 namespace CourseRegistrationAPIDemo
 {
     public class Program
     {
+        [Obsolete]
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
-            builder.Services.AddScoped<ICourseRegistrationService, CourseRegistrationService>();
+            builder.Services.AddControllers()
+                .AddFluentValidation(fv =>
+                {
+                    fv.RegisterValidatorsFromAssemblyContaining<CourseRegistrationCreateDtoValidator>();
+                });
+        builder.Services.AddScoped<ICourseRegistrationService,CourseRegistrationService>();
+         
+          
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddScoped<IValidator<CourseRegistrationCreateDto>, CourseRegistrationCreateDtoValidator>();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
